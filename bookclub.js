e2297e6ed4e1410fe1c1ba093a61b1e6cbb718c6
Sven Olson <lovickk@gmail.com>
2018-04-23 16:27:02 -0700

    Committing changes from like 3 days ago

--- bookclub.js
+++ bookclub.js
@@ -1,21 +1,21 @@
-
+var responseList = null;
 
 function newRequest() {
 
 	var title = document.getElementById("title").value;
 	title = title.trim();
-	title = title.replace(" ","+");
+	title = title.replace(" ", "+");
 
 	var author = document.getElementById("author").value;
 	author = author.trim();
-	author = author.replace(" ","+");
+	author = author.replace(" ", "+");
 
 	var isbn = document.getElementById("isbn").value;
 	isbn = isbn.trim();
-	isbn = isbn.replace("-","");
+	isbn = isbn.replace("-", "");
 
 
-	var query = ["",title,author,isbn].join("+");
+	var query = ["", title, author, isbn].join("+");
 	if (query != "") {
 
 		// remove old script
@@ -27,34 +27,55 @@ function newRequest() {
 		var script = document.createElement('script');
 
 		// build up complicated request URL
-		var beginning = "https://www.googleapis.com/books/v1/volumes?q="
-		var callback = "&callback=handleResponse"
+		var beginning = "https://www.googleapis.com/books/v1/volumes?q=";
+		var callback = "&callback=handleResponse";
 
-		script.src = beginning+query+callback	
+		script.src = beginning + query + callback;
 		script.id = "jsonpCall";
 
 		// put new script into DOM at bottom of body
-		document.body.appendChild(script);	
-		}
+		document.body.appendChild(script);
+	}
 
-}	
-	
-
-function handleResponse(bookListObj) {
-	var bookList = bookListObj.items;
-
-	/* where to put the data on the Web page */ 
-	var bookDisplay = document.getElementById("bookDisplay");
-
-	/* write each title as a new paragraph */
-	for (i=0; i<bookList.length; i++) {
-		var book = bookList[i];
-		var title = book.volumeInfo.title;
-		var titlePgh = document.createElement("p");
-		/* ALWAYS AVOID using the innerHTML property */
-		titlePgh.textContent = title;
-		bookDisplay.append(titlePgh);
-	}	
 }
 
 
+function handleResponse(bookListObj) {
+	responseList = bookListObj;
+	var bookList = responseList.items;
+	var displayedIndex = 0;
+
+	// where to put the data on the Web page
+	var popup = document.getElementById("popup");
+
+	// Populate the popup content and buttons
+	if (bookList.length == 0) {
+		// display the no results stuff and return
+	}
+
+	if (displayedIndex > 0) {
+		// display the left button
+	}
+
+	if (displayIndex < bookList.length - 1) {
+		// display the right button
+	}
+	popup.innerHTML = "<button id='goLeft'>left</button><button id='goRight'>right</button>";
+	var left = document.getElementById("goLeft");
+	var right = document.getElementById("goRight");
+
+	left.onclick = function() {
+		if (displayedIndex > 0) {
+
+		}
+	}
+	/* write each title as a new paragraph */
+	//for (i = 0; i < bookList.length; i++) {
+	//	var book = bookList[i];
+	//	var title = book.volumeInfo.title;
+	//	var titlePgh = document.createElement("p");
+	//	/* ALWAYS AVOID using the innerHTML property */
+	//	titlePgh.textContent = title;
+	//	bookDisplay.append(titlePgh);
+	//}
+}
