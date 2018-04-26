3097a201fd8751acd98a0817ce788adf33867bbb
Sven Olson <lovickk@gmail.com>
2018-04-25 19:10:22 -0700

    midway through implementing the invalid results

--- bookclub.js
+++ bookclub.js
@@ -55,6 +55,24 @@ function handleResponse(bookListObj) {
 		return ele;
 	}
 
+	function genDesc(desc) {
+		if (desc == undefined) {
+			return "";
+		}
+		var splitDesc = desc.split(" ");
+		var outStr = "";
+		var len = splitDesc.length;
+		if (len > 30) {
+			len = 30;
+		}
+		for (var i = 0; i < len; i++) {
+			outStr += splitDesc[i];
+			outStr += " ";
+		}
+		outStr += ". . .";
+		return outStr;
+	}
+
 	function generatePopup(index) {
 		// where to put the data on the Web page
 		var popup = document.createElement("div");
@@ -90,15 +108,17 @@ function handleResponse(bookListObj) {
 		var h2 = createEle("h2", "", volInfo.title);
 		innerDiv.appendChild(h2);
 		var authString = "";
-		for (var i = 0; i < volInfo.authors.length; i++) {
-			authString += volInfo.authors[i];
-			if (i+1 < volInfo.authors.length) {
-				authString += ", ";
+		if (volInfo.authors != undefined) {
+			for (var i = 0; i < volInfo.authors.length; i++) {
+				authString += volInfo.authors[i];
+				if (i+1 < volInfo.authors.length) {
+					authString += ", ";
+				}
 			}
 		}
 		var h3 = createEle("h3", "", authString);
 		innerDiv.appendChild(h3);
-		var p = createEle("p", "", "dfgjoisdjgf");
+		var p = createEle("p", "", genDesc(volInfo.description));
 		innerDiv.appendChild(p);
 
 		sec.appendChild(img);
@@ -131,11 +151,68 @@ function handleResponse(bookListObj) {
 			generatePopup(index+1);
 		}
 
+		keep.onclick = function () {
+			var tile = document.createElement("section");
+
+			var tImg = document.createElement("img");
+			tImg.src = img.src;
+			tImg.alt = volInfo.title;
+
+			tile.appendChild(tImg);
+
+			var tDiv = document.createElement("div");
+			var tClose = createEle("button", "", "ⓧ");
+			var tH2 = createEle("h2", "", volInfo.title);
+			var tH3 = createEle("h3", "", authString);
+			var tP = createEle("p", "", genDesc(volInfo.description));
+
+			tDiv.appendChild(tClose);
+			tDiv.appendChild(tH2);
+			tDiv.appendChild(tH3);
+			tDiv.appendChild(tP);
+
+			tile.appendChild(tDiv);
+
+			var tiles = document.getElementById("tiles");
+			if (tiles == null) {
+				var nav = document.getElementById("nav");
+
+				nav.style.alignItems = "flex-end";
+				nav.style.flex = "0 0 auto";
+				nav.style.flexDirection = "row";
+
+				nav.children[1].children[0].style.display = "none";
+				nav.children[1].style.flexGrow = "1";
+				nav.children[1].style.flexWrap = "nowrap";
+
+				tiles = createEle("main", "tiles", "");
+				document.body.appendChild(tiles);
+			}
+
+			tiles.appendChild(tile);
+
+			tClose.onclick = function () {
+				tile.remove();
+			}
+
+			popup.remove();
+		}
+
 		document.body.appendChild(popup);
 	}
 
 	if (bookListObj.totalItems == 0) {
-		// display the nothing found stuff instead
+		var popup = createEle("div", "popup", "");
+
+		var noresult = createEle("div", "noresult", "");
+
+		var p = document.createElement("p");
+		var str = "The book <strong>b</strong>";
+		var pT = document.createTextNode(str);
+		p.appendChild(pT);
+
+		document.body.appendChild(p);
+
 		return;
 	}
 
